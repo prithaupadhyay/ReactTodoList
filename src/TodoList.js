6a2fa446756a5aa468da68d08ffd297ecca02e67
prithaupadhyay <prithaupadhyay@gmail.com>
2019-03-11 14:48:28 +0530

    Added remove feature

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,6 +9,7 @@ class TodoList extends Component {
             items: []
           };
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
       }
        
       addItem(e) {
@@ -31,6 +32,17 @@ class TodoList extends Component {
              
           e.preventDefault();
       }
+
+      deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+          return (item.key !== key);
+        });
+       
+        this.setState({
+          items: filteredItems
+        });
+      }
+
   render() {
     return (
       <div className="todoListMain">
@@ -42,7 +54,7 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}/>
+        <TodoItems entries={this.state.items}delete={this.deleteItem}/>
       </div>
     );
   }
